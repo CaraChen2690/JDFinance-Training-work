852890e1d5d7471d56dc25e1427f2b5884e5bbfb
CaraChen <459718128@qq.com>
2018-05-21 22:28:10 +0800

    删除一些没用的文件

--- app/js/home/index.vue
+++ app/js/home/index.vue
@@ -1,38 +1,13 @@
 <template lang="html">
-    <div>
-        <Heador/>
-        <Slider/>
-        <Novice/>
-        <Borrow/>
-        <Money/>
-        <Product/>
-        <Life/>
-        <Footor/>
-        <Navbar/>
-    </div>
+    <Heador></Heador>
 </template>
 
 <script>
-import Heador from "../public/header.vue"
-import Footor from "../public/footer.vue"
-import Navbar from "../public/navbar.vue"
-import Slider from "./hslider.vue"
-import Novice from "./novice.vue"
-import Borrow from "./borrow.vue"
-import Money from "./money.vue"
-import Product from "./product.vue"
-import Life from "./life.vue"
+import Heador from "../public/header.vue";
 export default {
-    components: {
-        Heador,
-        Slider,
-        Novice,
-        Borrow,
-        Money,
-        Product,
-        Life,
-        Footor,
-        Navbar,
-    },
+	components: {
+		Heador,
+	},
 }
 </script>
+
